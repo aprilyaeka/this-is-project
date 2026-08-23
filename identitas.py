4d34e5d5107b2c5c632386b7cf71f781cba45706
aprilyaeka <aprilyaekanurfadilah@gmail.com>
2026-08-23 12:15:53 +0700

    Add files via upload

---
+++ identitas.py
@@ -0,0 +1,30 @@
+print("hello world!!");
+print("\t\tHalo Apa Kabar?");
+print("\n Selamat datang di Lab.Teknologi Informasi");
+print("\n Universitas Tidar");
+# menampilkan judul
+print("----------------------------------------------------\n\tDATA DIRI MAHASISWA\n----------------------------------------------------------")
+
+#menampilkan nama
+print("Nama \t\t\t\t\t : Aprilya Eka Nurfadilah")
+
+#menampilkan nim
+print("Nim \t\t\t\t\t\t: 2605060015")
+
+#menampilkan tempat,tanggal lahir
+print("Tempat,tanggal lahir\t :Bojonegoro,16 April 2007")
+
+#menampilkan jenis kelamin
+print("Jenis Kelamin \t\t\t :Perempuan")
+
+#menampilkan hobi
+print("Hobi \t\t\t\t\t : Menyanyi dan Membaca")
+
+#menampilkan garis penutup
+print("---------------------------------------------------------")
+
+
+
+
+
+
