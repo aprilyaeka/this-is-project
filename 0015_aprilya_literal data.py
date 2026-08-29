0edd11b46e50dcd263bc39f9c1b094cd7ce2f4c7
aprilyaeka <aprilyaekanurfadilah@gmail.com>
2026-08-30 00:24:47 +0700

    Add files via upload

---
+++ 0015_aprilya_literal data.py
@@ -0,0 +1,4 @@
+====================================================================================================================================
+print("nama = Aprilya Eka Nurfadilah", data_str, "type = ", type(data str))
+print("umur = 19 tahun", data_int "type = ", type(data_int))
+print("berat = ", data_bool, "type = ", type(data_bool))
